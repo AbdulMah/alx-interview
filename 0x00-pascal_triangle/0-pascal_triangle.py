9369f3ec35e566f9254941b9d1aea464e1282f94
Abdoulkarim <amahamn@gmail.com>
2022-12-21 23:58:14 +0100

    final

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """
-    0-pascal_triangle.py: pascal_triangle()
+    S 0-pascal_triangle.py: pascal_triangle()
 """
 
 
-def pascal_triangle(n):
+def pascal_triangle(n: int):
     """
         returns a lis of lists of integers
         Args:
             n (int): number of lists and digits
         Returns: list of lists
     """
-    t_row = [1]
-    temp_l = [0]
-    mylst = []
-
+    result = []
     if n <= 0:
-        return mylst
+        return result
 
-    for i in range(n):
-        mylst.append(t_row)
-        t_row = [l+r for l, r in zip(t_row + temp_l, temp_l + t_row)]
-    return mylst
+    for i in range(1, n + 1):
+        level = []
+        val = 1
+        for j in range(1, i + 1):
+            level.append(val)
+            val = val * (i - j) // j
+        result.append(level)
+    return result
