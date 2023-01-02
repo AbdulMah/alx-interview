1b8d21f68ed0306023e814a3086d9004cea73072
Abdoulkarim <amahamn@gmail.com>
2023-01-02 12:14:02 +0100

    final

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,6 +3,12 @@
 
 
 def minOperations(n):
+    """minOperations
+    Gets fewest of operations needed 
+
+    Args:
+        n (int): number
+    """
     num_ops, min_ops = 0, 2
     while n > 1:
         while n % min_ops == 0:
