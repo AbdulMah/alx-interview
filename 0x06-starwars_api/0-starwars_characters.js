4c1d498eeee8b34f5c8642d509d2d59477cd0a7b
Abdoulkarim <amahamn@gmail.com>
2023-01-02 12:24:32 +0100

    final

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, res, body) => {
+const req = request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, res, body) => {
   if (err) throw err;
   
   const actors = JSON.parse(body).characters;
@@ -11,7 +11,7 @@ request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, re
 
 const exactOrder = (actors, x) => {
   if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
+  req(actors[x], function (err, res, body) {
     if (err) throw err;
 
     console.log(JSON.parse(body).name);
