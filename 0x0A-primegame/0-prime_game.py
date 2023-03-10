022978f67e9ae3063a83f95819a0e204e45dcd05
Abdoulkarim <amahamn@gmail.com>
2023-03-10 01:22:13 +0100

    final

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -12,18 +12,15 @@ def isWinner(x, nums):
     n = max(nums)
     primes = [True for _ in range(1, n + 1, 1)]
     primes[0] = False
-    
     for i, is_prime in enumerate(primes, 1):
         if i == 1 or not is_prime:
             continue
         for j in range(i + i, n + 1, i):
             primes[j - 1] = False
-            
     for _, n in zip(range(x), nums):
         primes_count = len(list(filter(lambda x: x, primes[0: n])))
         bens_wins += primes_count % 2 == 0
         marias_wins += primes_count % 2 == 1
-
     if marias_wins == bens_wins:
         return None
     return 'Maria' if marias_wins > bens_wins else 'Ben'
